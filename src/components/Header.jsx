3f4ed2b6c33b36677aac825225d6b8dc2cf83012
Ozerov <noumager@gmail.com>
2020-11-30 22:42:41 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/a/a0/Firefox_logo%2C_2019.svg'></img>
+        </header>
+    );
+}
+
+export default Header;
